810fd2b9535337d3ad65fb7e6501026cd7f63f6c
Asmita Ramkrushna Borude <asmitaborude740@gmail.com>
2020-10-14 21:25:46 +0530

    #day10
    Python Object Oriented Programming
    Python Class and Object

---
+++ python_objectandclass.py
@@ -0,0 +1,194 @@
+#Creating Class and Object in Python
+class Parrot:
+
+    # class attribute
+    species = "bird"
+
+    # instance attribute
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+# instantiate the Parrot class
+blu = Parrot("Blu", 10)
+woo = Parrot("Woo", 15)
+
+# access the class attributes
+print("Blu is a {}".format(blu.__class__.species))
+print("Woo is also a {}".format(woo.__class__.species))
+
+# access the instance attributes
+print("{} is {} years old".format( blu.name, blu.age))
+print("{} is {} years old".format( woo.name, woo.age))
+
+#Creating Methods in Python
+class Parrot:
+
+    # instance attributes
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    # instance method
+    def sing(self, song):
+        return "{} sings {}".format(self.name, song)
+
+    def dance(self):
+        return "{} is now dancing".format(self.name)
+
+# instantiate the object
+blu = Parrot("Blu", 10)
+
+# call our instance methods
+print(blu.sing("'Happy'"))
+print(blu.dance())
+
+#Use of Inheritance in Python
+# parent class
+class Bird:
+
+    def __init__(self):
+        print("Bird is ready")
+
+    def whoisThis(self):
+        print("Bird")
+
+    def swim(self):
+        print("Swim faster")
+
+# child class
+class Penguin(Bird):
+
+    def __init__(self):
+        # call super() function
+        super().__init__()
+        print("Penguin is ready")
+
+    def whoisThis(self):
+        print("Penguin")
+
+    def run(self):
+        print("Run faster")
+
+peggy = Penguin()
+peggy.whoisThis()
+peggy.swim()
+peggy.run()
+
+#Data Encapsulation in Python
+class Computer:
+
+    def __init__(self):
+        self.__maxprice = 900
+
+    def sell(self):
+        print("Selling Price: {}".format(self.__maxprice))
+
+    def setMaxPrice(self, price):
+        self.__maxprice = price
+
+c = Computer()
+c.sell()
+
+# change the price
+c.__maxprice = 1000
+c.sell()
+
+# using setter function
+c.setMaxPrice(1000)
+c.sell()
+
+#Using Polymorphism in Python
+class Parrot:
+
+    def fly(self):
+        print("Parrot can fly")
+
+    def swim(self):
+        print("Parrot can't swim")
+
+class Penguin:
+
+    def fly(self):
+        print("Penguin can't fly")
+
+    def swim(self):
+        print("Penguin can swim")
+
+# common interface
+def flying_test(bird):
+    bird.fly()
+
+#instantiate objects
+blu = Parrot()
+peggy = Penguin()
+
+# passing the object
+flying_test(blu)
+flying_test(peggy)
+
+#defining a class in python
+class Person:
+    "This is a person class"
+    age = 10
+
+    def greet(self):
+        print('Hello')
+
+
+# Output: 10
+print(Person.age)
+
+# Output: <function Person.greet>
+print(Person.greet)
+
+# Output: 'This is my second class'
+print(Person.__doc__)
+
+#creating an object in python
+class Person:
+    "This is a person class"
+    age = 10
+
+    def greet(self):
+        print('Hello')
+
+
+# create a new object of Person class
+harry = Person()
+
+# Output: <function Person.greet>
+print(Person.greet)
+
+# Output: <bound method Person.greet of <__main__.Person object>>
+print(harry.greet)
+
+# Calling object's greet() method
+# Output: Hello
+harry.greet()
+
+#constructors in python
+class ComplexNumber:
+    def __init__(self, r=0, i=0):
+        self.real = r
+        self.imag = i
+
+    def get_data(self):
+        print(f'{self.real}+{self.imag}j')
+
+
+# Create a new ComplexNumber object
+num1 = ComplexNumber(2, 3)
+
+# Call get_data() method
+# Output: 2+3j
+num1.get_data()
+
+# Create another ComplexNumber object
+# and create a new attribute 'attr'
+num2 = ComplexNumber(5)
+num2.attr = 10
+
+# Output: (5, 0, 10)
+print((num2.real, num2.imag, num2.attr))
+
