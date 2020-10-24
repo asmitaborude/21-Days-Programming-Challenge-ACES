7e653da5745d95d3ba096c96027a268cfcb7f455
Asmita Ramkrushna Borude <asmitaborude740@gmail.com>
2020-10-24 23:18:24 +0530

    #day19
    python datetime module

---
+++ dateandtime2.py
@@ -0,0 +1,89 @@
+#Difference between two dates and times
+
+from datetime import datetime, date
+
+t1 = date(year = 2018, month = 7, day = 12)
+t2 = date(year = 2017, month = 12, day = 23)
+t3 = t1 - t2
+print("t3 =", t3)
+
+t4 = datetime(year = 2018, month = 7, day = 12, hour = 7, minute = 9, second = 33)
+t5 = datetime(year = 2019, month = 6, day = 10, hour = 5, minute = 55, second = 13)
+t6 = t4 - t5
+print("t6 =", t6)
+
+print("type of t3 =", type(t3))
+print("type of t6 =", type(t6))
+
+# Difference between two timedelta objects
+
+from datetime import timedelta
+
+t1 = timedelta(weeks = 2, days = 5, hours = 1, seconds = 33)
+t2 = timedelta(days = 4, hours = 11, minutes = 4, seconds = 54)
+t3 = t1 - t2
+
+print("t3 =", t3)
+
+#Printing negative timedelta object
+
+from datetime import timedelta
+
+t1 = timedelta(seconds = 33)
+t2 = timedelta(seconds = 54)
+t3 = t1 - t2
+
+print("t3 =", t3)
+print("t3 =", abs(t3))
+
+#time duration in second
+from datetime import timedelta
+
+t = timedelta(days = 5, hours = 1, seconds = 33, microseconds = 233423)
+print("total seconds =", t.total_seconds())
+
+# Format date using strftime()
+
+from datetime import datetime
+
+# current date and time
+now = datetime.now()
+
+t = now.strftime("%H:%M:%S")
+print("time:", t)
+
+s1 = now.strftime("%m/%d/%Y, %H:%M:%S")
+# mm/dd/YY H:M:S format
+print("s1:", s1)
+
+s2 = now.strftime("%d/%m/%Y, %H:%M:%S")
+# dd/mm/YY H:M:S format
+print("s2:", s2)
+
+#strptime()
+
+from datetime import datetime
+
+date_string = "21 June, 2018"
+print("date_string =", date_string)
+
+date_object = datetime.strptime(date_string, "%d %B, %Y")
+print("date_object =", date_object)
+
+
+#handling timezone in python
+from datetime import datetime
+import pytz
+
+local = datetime.now()
+print("Local:", local.strftime("%m/%d/%Y, %H:%M:%S"))
+
+
+tz_NY = pytz.timezone('America/New_York')
+datetime_NY = datetime.now(tz_NY)
+print("NY:", datetime_NY.strftime("%m/%d/%Y, %H:%M:%S"))
+
+tz_London = pytz.timezone('Europe/London')
+datetime_London = datetime.now(tz_London)
+print("London:", datetime_London.strftime("%m/%d/%Y, %H:%M:%S"))
+
